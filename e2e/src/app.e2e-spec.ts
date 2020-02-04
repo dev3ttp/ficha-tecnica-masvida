fdc30a2c5aa5cea806c6973b1bc6d75d7bad1024
Totalpack1 <felipe.rios@totalpack.cl>
2020-02-04 17:20:37 -0300

    ficha ok

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('fichaTecnica app is running!');
+    expect(page.getTitleText()).toEqual('ficha-tecnica-masvida app is running!');
   });
 
   afterEach(async () => {
